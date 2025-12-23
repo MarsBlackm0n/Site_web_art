9708211635bcb591681ee1b100cb37a0e92e6482
MarsBlackm0n <131268973+MarsBlackm0n@users.noreply.github.com>
2025-12-23 18:31:30 +0000

    Scrapi

---
+++ cms/src/api/artwork/index.ts
@@ -0,0 +1,9 @@
+import router from "./routes/artwork";
+import controller from "./controllers/artwork";
+import service from "./services/artwork";
+
+export default {
+  router,
+  controller,
+  service,
+};
