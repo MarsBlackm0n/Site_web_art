b037a0b4edcde6001b79841a69989680a7e63b11
MarsBlackm0n <131268973+MarsBlackm0n@users.noreply.github.com>
2025-12-25 17:36:51 +0000

    cloudflare public

--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -3,9 +3,11 @@ export default ({ env }) => ({
     config: {
       provider: "aws-s3",
       providerOptions: {
+        baseUrl: env("R2_PUBLIC_BASE_URL"),
         s3Options: {
           endpoint: env("R2_ENDPOINT"),
           region: env("R2_REGION", "auto"),
+          forcePathStyle: true,
           credentials: {
             accessKeyId: env("R2_ACCESS_KEY_ID"),
             secretAccessKey: env("R2_SECRET_ACCESS_KEY"),
