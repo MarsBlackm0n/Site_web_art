d3fb28c0178912d8294fbc77ea6bccd76488c7c6
MarsBlackm0n <131268973+MarsBlackm0n@users.noreply.github.com>
2025-12-25 17:27:01 +0000

    cloudflare R2

--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -1,1 +1,20 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        s3Options: {
+          endpoint: env("R2_ENDPOINT"),
+          region: env("R2_REGION", "auto"),
+          credentials: {
+            accessKeyId: env("R2_ACCESS_KEY_ID"),
+            secretAccessKey: env("R2_SECRET_ACCESS_KEY"),
+          },
+        },
+        params: {
+          Bucket: env("R2_BUCKET"),
+        },
+      },
+    },
+  },
+});
